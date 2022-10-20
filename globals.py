36da559f35e5df7c5456401332fca0ac5c26ae2a
Zigrino <yulian.zigrino@gmail.com>
2022-10-19 22:34:03 -0400

    Added window

---
+++ globals.py
@@ -0,0 +1,1 @@
+window_dimensions = [800, 600]
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+from operator import truediv
 import pygame
-
+import globals
+import sys
+pygame.init()
+WIDTH = globals.window_dimensions[0]
+HEIGHT = globals.window_dimensions[1]
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 def main():
-    pass
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-if __name__ == "__main__":
+if  __name__ == "__main__":
     main()
