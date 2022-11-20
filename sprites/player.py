9635d33938eba0cd294a357febfb47e668b4632b
Zigrino <yulian.zigrino@gmail.com>
2022-11-20 16:50:55 -0500

    fixed scaling

--- sprites/player.py
+++ sprites/player.py
@@ -2,9 +2,12 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load(r"assets/images/Temperary_Dude.png")
+        self.image = pygame.image.load(r"assets/images/Temporary_Dude.png")
+        # 11000 × 11000
+        self.scale = 0.1
+        self.image = pygame.transform.scale(self.image, (11000*self.scale, 11000*self.scale))
         self.rect = self.image.get_rect()
-        self.rect.midleft = (0, 300)
+        self.rect.center = (0, 300)
     def draw(self, surface):
         surface.blit(self.image, self.rect)
     def update(self):
--- sprites/sword.py
+++ sprites/sword.py
@@ -5,11 +5,12 @@ class Sword(pygame.sprite.Sprite):
         super().__init__()
         self.player = player;
         self.image_list = [pygame.image.load(r"assets/images/Sword.png"), pygame.image.load(r"assets/images/Bloody_Sword.png")]
-        self.scale = 0.5
+        self.scale = 0.1
         #Original image dimensions = 448x448
         self.image = self.image_list[0]
+        self.image = pygame.transform.scale(self.image, (448*self.scale, 448*self.scale))
         self.rect = self.image.get_rect()
-        self.rect.midleft = player.rect.midright
+        self.rect.center = player.rect.center
         self.surface = pygame.display.get_surface()
     def draw(self, surface):
         surface.blit(self.image, self.rect)
